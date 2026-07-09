f0aae2839b2e4de31ada7b5cf70a6bef73ce209f
Subham Kumar Padhy <presentpastfuture19@gmail.com>
2026-07-09 12:12:47 +0530

    Fix typo in variable name from 'input_penguines' to 'input_penguins'

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ with st.sidebar:
          'body_mass_g':body_mass_g,
          'sex':gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguines = pd.concat([input_df, x], axis=0)
+  input_penguins = pd.concat([input_df, x], axis=0)
 
 with st.expander('Input features'):
   st.write('**Input penguin**')
